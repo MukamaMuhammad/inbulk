52785043e46ce07f7aac07cf6070f5288cfc832b
lolenti <lolentimuhammad2@gmail.com>
2024-01-25 13:30:03 +0300

    Google analytics added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section class="text-gray-600 body-font">
+    <section class="text-gray-600 body-font h-[80vh]">
       <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
